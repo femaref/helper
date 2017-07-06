173dc125710052f404f4c1bb2aa43e0dd52578f4
Femaref <femaref@googlemail.com>
2017-07-06 11:46:13 +0200

    map/struct

---
+++ mapstruct/mapstruct.go
@@ -0,0 +1,54 @@
+package mapstruct
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"encoding/json"
+)
+
+func setField(obj interface{}, name string, value interface{}) error {
+	structValue := reflect.ValueOf(obj).Elem()
+	structFieldValue := structValue.FieldByName(name)
+
+	if !structFieldValue.IsValid() {
+		return fmt.Errorf("No such field: %s in obj", name)
+	}
+
+	if !structFieldValue.CanSet() {
+		return fmt.Errorf("Cannot set %s field value", name)
+	}
+
+	structFieldType := structFieldValue.Type()
+	val := reflect.ValueOf(value)
+	if structFieldType != val.Type() {
+		invalidTypeError := errors.New("Provided value type didn't match obj field type")
+		return invalidTypeError
+	}
+
+	structFieldValue.Set(val)
+	return nil
+}
+
+func MapToStruct(s interface{}, m map[string]interface{}) error {
+	for k, v := range m {
+		err := setField(s, k, v)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func StructToMap(s interface{}) (map[string]interface{}, error) {
+    marsh, err := json.Marshal(s)
+
+    if err != nil {
+        return nil, err
+    }
+
+    var unmarsh map[string]interface{}
+
+    err = json.Unmarshal(marsh, &unmarsh)
+
+    return unmarsh, err
+}
